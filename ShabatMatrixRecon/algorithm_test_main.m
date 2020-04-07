edb7b6d55e8a2fd62c1ed37fa2345776d8953b86
Jaeyoon Kim <jaeykim@umich.edu>
2020-04-06 23:44:31 -0400

    Major Refactoring - Took out reconstruction part of the matrix as a separate function         - masked_reconstruction and all_zero_reconstruction functions - Added option to silence the plots

--- ShabatMatrixRecon/algorithm_test_main.m
+++ ShabatMatrixRecon/algorithm_test_main.m
@@ -15,60 +15,27 @@ zero_inflation_mask = rand(size(ground_truth)) < zero_inflate_prob; % 0%  of ent
 observed = ground_truth;
 observed(zero_inflation_mask) = 0;
 
-%% Collect the set of zero entries
-zero_entries = (observed == 0);
-zero_inds = find(zero_entries);
-num_zeros = length(zero_inds);
+%% Reconstruct from "Observation" matrix
+num_mask = 2;
 
-%% Make masks
-num_masks = 2;
-
-theoretical_limit = 0.75; % Fix this to be something from CBT paper
-num_non_observed = min(floor(num_zeros * 0.9),floor(theoretical_limit*numel(ground_truth)));
-
-masks = cell(num_masks,1);
-
-% Tried using all the zeros as the "unobserved population"
-for i = 1:num_masks
-    masks{i} = zero_inds(randperm(num_zeros,num_non_observed));
-%     masks{i} = find(zero_inflation_mask); 
-end
-
-%% Reconstruct matrix with each mask (parallel step)
-recon_array = cell(num_masks,1);
-
-lamnbda_tol = 10;
+lambda_tol = 10;
 tol = 1e-8;
 N = 100;
-parfor j = 1:num_masks
-    mask_matrix = zeros(num_genes,num_cells);
-    mask_matrix(masks{j}) = 1;
     
-    observed_entries = logical(1 - mask_matrix);
-    
-    [CompletedMat, ier] = MatrixCompletion(...
-        observed, observed_entries,N, 'nuclear', lamnbda_tol, tol, 0);
-    
-    recon_array{j} = CompletedMat;
-end
+% reconstructed = masked_reconstruction(observed, num_mask, lambda_tol, tol, N);
+% or 
+% reconstructed = all_zero_reconstruction(observed, lambda_tol, tol, N);
 
-%% Arrange reconstructed matrices into an array for easy median-computation
-recon_array_arrangement = zeros(num_masks, num_genes * num_cells); 
-for i = 1:num_masks
-    recon_array_arrangement(i,:) = recon_array{i}(:);
-end 
+reconstructed = masked_reconstruction(observed, num_mask, lambda_tol, tol, N);
 
-%% Form putative matrix 
-putative_array_matrix = zeros(num_genes,num_cells); 
-putative_array_matrix(:) = median(recon_array_arrangement,1); 
-
-%% 
+%% Print performance results
+zero_entries = (observed == 0);
 recon_mask = logical(zero_entries); 
 known_mask = 1 - recon_mask; 
 
 fprintf('\n Corrupted matrix nuclear norm (initial): %g \n',sum(svd(observed)));
 fprintf('Restored matrix nuclear norm (final): %g \n',sum(svd(putative_array_matrix)));
-Diff_sq = abs(putative_array_matrix - ground_truth).^2;
+Diff_sq = abs(reconstructed - ground_truth).^2;
 
 fprintf('MSE on known entries: %g \n',sqrt(sum2(Diff_sq .*known_mask) / sum(known_mask(:)) ));
 fprintf('MSE on unknown entries: %g \n',sqrt(sum2(Diff_sq.*recon_mask)/sum(recon_mask(:)) ));
@@ -76,23 +43,28 @@ fprintf('MSE on unknown entries: %g \n',sqrt(sum2(Diff_sq.*recon_mask)/sum(recon
 num_clusters = 10;
 fprintf('clustering perf of ground truth: %g \n ', clustering_perf(ground_truth, num_clusters))
 fprintf('clustering perf of observed: %g \n ', clustering_perf(observed, num_clusters))
-fprintf('clustering perf of reconstructed: %g \n ', clustering_perf(putative_array_matrix, num_clusters))
-%% 
-figure(1)
-hold on
-histogram(Diff_sq .* recon_mask,1000); 
+fprintf('clustering perf of reconstructed: %g \n ', clustering_perf(reconstructed, num_clusters))
+%% Plot results
+do_plot = false;
 
-figure(2)
-hold on
-histogram(putative_array_matrix .* known_mask,100);
+if do_plot
+    figure(1)
+    hold on
+    histogram(Diff_sq .* recon_mask,1000); 
 
-%%
+    figure(2)
+    hold on
+    histogram(putative_array_matrix .* known_mask,100);
 
-figure(3)
-hold on
-histogram(Diff_sq .* known_mask,100);
+
+    figure(3)
+    hold on
+    histogram(Diff_sq .* known_mask,100);
+end
 
 %% Helper Functions
+
+
 % Computes the average Silhoutte coeficient on k clustering (clustering
 % found by k means)
 % X (n x p matrix) each column represents a single point
@@ -111,4 +83,61 @@ function perf = clustering_perf(X, k)
             perf = trial_perf;
         end
     end
+end
+
+% Given an observed matrix, each zero entry has a chance of being set to
+% "unknown" element. These unknown elements computed by minimized nuclear
+% norm * const + l2 norm
+function reconstructed = masked_reconstruction(observed, num_masks, lambda_tol, tol, N)
+    %% Collect the set of zero entries
+    zero_entries = (observed == 0);
+    zero_inds = find(zero_entries);
+    num_zeros = length(zero_inds);
+    [num_genes, num_cells] = size(observed);
+
+    %% Make masks
+    theoretical_limit = 0.75; % Fix this to be something from CBT paper
+    num_non_observed = min(floor(num_zeros * 0.9),floor(theoretical_limit*numel(observed)));
+
+    masks = cell(num_masks,1);
+
+    % Tried using all the zeros as the "unobserved population"
+    for i = 1:num_masks
+        masks{i} = zero_inds(randperm(num_zeros,num_non_observed));
+    %     masks{i} = find(zero_inflation_mask); 
+    end
+
+    %% Reconstruct matrix with each mask (parallel step)
+    recon_array = cell(num_masks,1);
+
+    parfor j = 1:num_masks
+        mask_matrix = zeros(num_genes,num_cells);
+        mask_matrix(masks{j}) = 1;
+
+        observed_entries = logical(1 - mask_matrix);
+
+        [CompletedMat, ier] = MatrixCompletion(...
+            observed, observed_entries,N, 'nuclear', lambda_tol, tol, 0);
+
+        recon_array{j} = CompletedMat;
+    end
+
+    %% Arrange reconstructed matrices into an array for easy median-computation
+    recon_array_arrangement = zeros(num_masks, num_genes * num_cells); 
+    for i = 1:num_masks
+        recon_array_arrangement(i,:) = recon_array{i}(:);
+    end 
+
+    %% Form putative matrix 
+    reconstructed = zeros(num_genes,num_cells); 
+    reconstructed(:) = median(recon_array_arrangement,1); 
+
+end
+
+% Given an observed matrix, every zero entry is set as an "unknown" 
+% element. These unknown elements computed by minimized nuclear
+% norm * const + l2 norm
+function reconstructed = all_zero_reconstruction(observed, lambda_tol, tol, N)
+    [reconstructed, ier] = MatrixCompletion(...
+            observed, observed ~= 0,N, 'nuclear', lambda_tol, tol, 0);
 end
