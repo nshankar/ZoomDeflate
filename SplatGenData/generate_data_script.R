f7b5f39770d475be120116a15c51c45879ce2f9c
Nikhil Shankar <nikhil@Nikhils-MacBook-Air.local>
2020-04-07 12:11:46 -0400

    Added 2 sets of 50% sparse synthetic data

--- SplatGenData/generate_data_script.R
+++ SplatGenData/generate_data_script.R
@@ -4,10 +4,12 @@ library(splatter)
 random_seed <- 43
 num_cells <- 1000
 num_genes <- 50
-num_groups <- 2
+num_groups <- 1
 dropout <- 1 #boolean valued for now
+dropout.mid.value <- 6
+dropout.shape.value <- -0.5
 
-PATHNAME <- "/Users/nikhil/Documents/College/Math 651/generate_data/example_data/"
+PATHNAME <- "/Users/nikhil/Documents/College/Math 651/ZoomDeflate/SplatGenData/one_cell_types_50_sparse/"
 
 ### Run simulation ###
 params <- newSplatParams()
@@ -19,8 +21,8 @@ if(num_groups > 1) {
   probabilities = rep(1/num_groups, num_groups)
   params <- setParam(params, "group.prob", probabilities)
   if (dropout) {
-    dropout.mid = rep(0, num_groups) #default, possibly a bad assumption
-    dropout.shape = rep(-1, num_groups) #default, possibly a bad assumption
+    dropout.mid = rep(dropout.mid.value, num_groups) #default, possibly a bad assumption
+    dropout.shape = rep(dropout.shape.value, num_groups) #default, possibly a bad assumption
     params <- setParam(params, "dropout.mid", dropout.mid)
     params <- setParam(params, "dropout.shape", dropout.shape)
     params <- setParam(params, "dropout.type", "group")
@@ -28,10 +30,14 @@ if(num_groups > 1) {
   sim <- splatSimulate(params, method = "group")
 } else {
   if (dropout) {
+    params <- setParam(params, "dropout.mid", dropout.mid.value)
+    params <- setParam(params, "dropout.shape", dropout.shape.value)
     params <-setParam(params, "dropout.type", "experiment")
   }
   sim <- splatSimulate(params)
 }
+
+sum(rowSums(counts(sim) == 0))
 
 ### Save data and metadata (which metadata is important?) ###
 dir.create(PATHNAME)
