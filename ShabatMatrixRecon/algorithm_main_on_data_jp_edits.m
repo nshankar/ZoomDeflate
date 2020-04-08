b07f509f538f4ad379f4c65bdf81bf016e0211e7
Jeremy D'Silva <jpdsilva@umich.edu>
2020-04-08 18:39:15 -0400

    caught a bug in the bugfix

--- ShabatMatrixRecon/algorithm_main_on_data_jp_edits.m
+++ ShabatMatrixRecon/algorithm_main_on_data_jp_edits.m
@@ -174,43 +174,43 @@ end
 %% Helper Functions
 
 % computes RMSE of entries included in the mask. mask should be logical.
-function [RMSE_value,scalar_multiples] = RMSE(X,truth,mask)
+function RMSE_value = RMSE(X,truth,mask)
     squared_diff = (X - truth).^2;
     RMSE_value = sqrt(sum2(squared_diff(mask)) / sum(mask(:)));
 end
 
-function X_proc = preprocess(X,scaling_flag)
+function [X_proc,scalar_multiples] = preprocess(X,scaling_flag)
 %Want to take stds only over the nonzero entries (NOT  row_stds =
-%std(X,0,2);)  
-scalar_multiples = eye(num_genes);
-if strcmp(scaling_flag,'std') || strcmp(scaling_flag,'noise')
-    num_genes = size(X,1); 
-    row_stds = zeros(num_genes,1);
-    for i = 1:num_genes
-        nonzero_entries = double(~(X(i,:)==0));
-        row_stds(i) = std(X(i,:),nonzero_entries,2); 
-    end 
-end
-if strcmp(scaling_flag,'none')
-    X_proc = X; 
-    return
-elseif strcmp(scaling_flag,'std')
-    scalar_multiples = diag( 1 ./ row_stds); 
-    X_proc = scalar_multiples * X; 
-    return
-elseif strcmp(scaling_flag,'noise')
-    row_means = zeros(num_genes,1);
-    for i = 1:num_genes
-        row_means(i) = mean(X(i,(X(i,:)~=0)));
-    end 
-    row_noise = (row_stds ./ row_means).^2; 
-    scalar_multiples = diag(1 ./ row_noise);
-    X_proc =  scalar_multiples * X; 
-    return
-else
-    fprintf('ERROR: INCORRECT FLAG IN PREPROCESSING. NO PROCESSING FOR YOU.')
-    X_proc = X; 
-end
+    %std(X,0,2);)
+    num_genes = size(X,1);
+    scalar_multiples = eye(num_genes);
+    if strcmp(scaling_flag,'std') || strcmp(scaling_flag,'noise')
+        row_stds = zeros(num_genes,1);
+        for i = 1:num_genes
+            nonzero_entries = double(~(X(i,:)==0));
+            row_stds(i) = std(X(i,:),nonzero_entries,2);
+        end
+    end
+    if strcmp(scaling_flag,'none')
+        X_proc = X;
+        return
+    elseif strcmp(scaling_flag,'std')
+        scalar_multiples = diag( 1 ./ row_stds);
+        X_proc = scalar_multiples * X;
+        return
+    elseif strcmp(scaling_flag,'noise')
+        row_means = zeros(num_genes,1);
+        for i = 1:num_genes
+            row_means(i) = mean(X(i,(X(i,:)~=0)));
+        end
+        row_noise = (row_stds ./ row_means).^2;
+        scalar_multiples = diag(1 ./ row_noise);
+        X_proc =  scalar_multiples * X;
+        return
+    else
+        fprintf('ERROR: INCORRECT FLAG IN PREPROCESSING. NO PROCESSING FOR YOU.')
+        X_proc = X;
+    end
 end
 
 
