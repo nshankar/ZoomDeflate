45101d3371f62bde33646a803cf148467878cbf4
Jaeyoon Kim <jaeykim@umich.edu>
2020-04-10 01:00:29 -0400

    removed old functions and prepared to import splatter data

--- TechnicalZeroDetector/ClusterSpan.py
+++ TechnicalZeroDetector/ClusterSpan.py
@@ -9,110 +9,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 
-def reconstruct(X, row_k, col_k,threshold = 0.5):
-    col_kmeans = KMeans(n_clusters=row_k).fit(X.T)
-    row_kmeans = KMeans(n_clusters=col_k).fit(X)
-    for row_label in range(row_k):
-        for col_label in range(col_k):
-            in_col = col_kmeans.labels_ == col_label
-            in_row = row_kmeans.labels_ == row_label
-            region = np.matmul(in_row.reshape(20,1), in_col.reshape(1,25))
-            num_in_region = sum(sum(region))
-            num_one = sum(X[region] == 1)
-            if num_one >= threshold * num_in_region:
-                X[region] = 1
-                
-    print("cols", col_kmeans.labels_)
-    print("rows", row_kmeans.labels_)
-    return X
+"""
+[Input]
+X: 0-1 matrix. Column for each cell, row for each gene
+    X[g,c] = 1 indicates that gene g was observed in cell c
+rank: Upper bound on number of cell types/clusters expected
+threshold_ratio: this is a hyper-parameter. Ensure that the threshold_ratio
+    is less than the 1 - dropoff rate. 
+    E.g. for sparcity 50% following seemed to work well
+    you need to play around with it
+    dropoff 50% threshold_ratio = 0.4
+    dropoff 70% threshold_ratio = 0.2
+    
+[Output]
+[TightMask, LooseMask, classification]
+Masks - 0 indicates that we predict that it is a true zero
+TightMask is more likely to output 0 
+    (less chance of falsely classifying techinical zero as a biological zero)
+LooseMask is more likely to output 1 
+    (more likely to detect technical zeros)
 
-# Gene ranking by average value of inner product of columns given
-# it has this gene divided by avg inner prod when its off
-def conditional_info_score(X):
-    # TODO normalize!
-    InnerProd = np.matmul(X.T, X)
-    [r, c] = np.shape(X)
-    score = np.zeros(r)
-    for gene in range(r):
-        has_gene = X[gene,:] != 0
-        if sum(has_gene) <= 1:
-            continue #set score to zero
-        sim_gene_on = 0
-        sim_gene_on_pair_count = 0
-        sim_gene_off = 0
-        sim_gene_off_pair_count = 0
-        for i in range(c):
-            for j in range(i+1,c):
-                if not (has_gene[i] or has_gene[j]):
-                    sim_gene_off += InnerProd[i,j]
-                    sim_gene_off_pair_count += 1
-                    #print("off",gene,i,j)
-                elif has_gene[i] and has_gene[j]:
-                    sim_gene_on += InnerProd[i,j]
-                    sim_gene_on_pair_count += 1
-                    #print("on",gene,i,j)
-        score[gene] = (sim_gene_on /sim_gene_on_pair_count)\
-            / (sim_gene_off / sim_gene_off_pair_count)
-        
-    return score
-    
-    
-# k number of weights
-def AdaBoost(X, col_label, k):
-    
-    [r, c] = np.shape(X)
-    weights = np.ones(c) / c
-    row_score = np.zeros(r)
-    for i in range(r):
-        row = X[i,:]
-        adjusted_label = col_label
-        adjusted_label[row == 0] *= -1
-        one_weights = [sum(weights.T[adjusted_label == l]) 
-                       for l in range(k)]
-        # one_weights[l] is the sum of weights that are 1 on the row
-        # and labeled l
-        max_one_label = np.argmax(one_weights)
-        zero_weights = [sum(weights[adjusted_label == -l]) 
-                       for l in range(k)]
-        max_zero_label = np.argmax(zero_weights)
-        
-        ## Compute error
-        # Weights add up to 1 so just subtract weights we get correct
-        err = 1 - one_weights[max_one_label] - zero_weights[max_zero_label]
-        row_score[i] = np.log((1 - err) / err) + np.log(k - 1)
-        
-        ## Update Weights
-        for j in range(c):
-            # If we get the correct label, we don't need to update
-            if row[j] == 0:
-                if col_label[j] == max_zero_label:
-                    continue
-            else:
-                if col_label[j] == max_one_label:
-                    continue
-                
-            weights[j] = weights[j] * np.exp(row_score[i])
-            
-        ## Normalize weights
-        weights = weights / sum(weights)
-        
-    print(row_score)
-    return row_score
-        
-#random 0-1 mat of rank at most rank
-#Warning! not very random, just for testing
-def random_mat(r,c,rank,sparcity):
-    n = int(np.floor(c/rank))
-    X = np.zeros([r,c])
-    for i in range(rank):
-        rand_col = 1 * (np.random.rand(r,1) > sparcity)
-        for j in range(n):
-            X[:,i * n + j] = rand_col.T
-            
-    return X
+cluster_span finds rank many vectors V_1, V_2,..., V_rank 
+that spans the image of the matrix X
+It ensures that for each cell, there is a V_i such that genes observed
+in the cell is a subset of genes corresponding to V_i.
+This function attempts to minimizes the number of zeros of each spanning vectors
+such that each cell fits in very "tight" to some spanning vector V_i
 
-def min_span(X, rank):
-    threshold_ratio = 0.4 # TODO fix this
+
+"""
+def cluster_span(X, rank, threshold_ratio):
     [r,c] = np.shape(X)
     # Run kmeans to get a general idea of clustering
     kmeans = KMeans(n_clusters=rank).fit(X.T)
@@ -156,42 +82,55 @@ def min_span(X, rank):
                 span_vec[best_vec][k] = 1
                 
     # Now classify points using spanning vectors and complete X
-    X_completed = np.zeros([r,c])
+    LooseMask = np.zeros([r,c])
+    TightMask = np.ones([r,c])
+    
     classification = np.zeros(c)
     for i in range(c):
-        #TODO below for loop is duplicated. Refactor!
+        #TODO below for loop is duplicated. Can I avoid passing by value?
         best_vec = -1
         best_score = float("-inf")
-        #num_span_fit = 0
         for j in range(rank):
             if not (X[:,i] <= span_vec[j]).all():
                 continue
-            #num_span_fit += 1
+            
+            TightMask[span_vec[j] == non_one_val,i] = 0
             
             score = np.dot(X[:,i], span_vec[j])
             if score > best_score:
                 best_score = score
                 best_vec = j
-        #print(num_span_fit)
         if best_vec == -1:
-            print("error!")
+            print("ERROR! Spanning set does not actually span")
         classification[i] = best_vec
-        X_completed[:,i] = span_vec[best_vec]
+        LooseMask[:,i] = span_vec[best_vec]
         
-    return classification, X_completed
+    return TightMask, LooseMask, classification
         
 
-def main():
+#random 0-1 mat of rank at most rank
+#Warning! not very random, just for testing
+def random_mat(r,c,rank,sparcity):
+    n = int(np.floor(c/rank))
+    X = np.zeros([r,c])
+    for i in range(rank):
+        rand_col = 1 * (np.random.rand(r,1) > sparcity)
+        for j in range(n):
+            X[:,i * n + j] = rand_col.T
+            
+    return X
+
+def test():
     row = 50
     col = 450
     rank = 6
-    dropoff = 0.5
+    dropoff = 0.7
     sparcity = 0.5
     X = random_mat(row, col, rank, sparcity)
     cor_mat = np.random.rand(row, col)
     X_corrupted = np.zeros([row, col])
     X_corrupted[cor_mat > dropoff] = X[cor_mat > dropoff]
-    classification, X_completed = min_span(X_corrupted, rank)
+    TightMask, LooseMask, classification = cluster_span(X_corrupted, rank, 0.2)
     
     kmeans = KMeans(n_clusters=rank).fit(X_corrupted.T)
     X_weighted = X_corrupted.copy()
@@ -200,16 +139,30 @@ def main():
     kmeans_weighted = KMeans(n_clusters=rank).fit(X_weighted.T)
     
     
+    print(np.shape(TightMask))
     
-    
-    fig, axs = plt.subplots(5)
+    fig, axs = plt.subplots(6)
     fig.suptitle('0-1 reconstruction')
     axs[0].imshow(kmeans.labels_.reshape(col,1).T)
     axs[1].imshow(classification.reshape(col,1).T)
     axs[2].imshow(X)
     axs[3].imshow(X_corrupted)
-    axs[4].imshow(X_completed)
+    axs[4].imshow(LooseMask)
+    axs[5].imshow(TightMask)
     plt.savefig("0-1_reconstruction", dpi=600)
+
+def main():
+    X = # TODO import 0-1 matrix
+    threshold_ratio = # TODO take as argument see description in documentation
+    rank = # TODO take as an argument (should be an upper bound on clustering)
+    
+    TightMask, LooseMask, classification = cluster_span(X, rank, threshold_ratio)
+    
+    # TODO save TightMask LooseMask
+    # TODO print some performance stuff? Number of technical zeros detected (hopefully)
+    # Also, it might be nice to see what happens when we reorder X and masks
+    # based on classification of cells to see how things line up
+    
     
 
 if __name__ == '__main__':
