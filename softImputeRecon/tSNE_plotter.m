776b555c30055f8d806cc4a2f171813ad9261daf
Jeremy D'Silva <jpdsilva@umich.edu>
2020-04-18 14:49:55 -0400

    cleaned up file loading and worked on tSNE

--- softImputeRecon/tSNE_plotter.m
+++ softImputeRecon/tSNE_plotter.m
@@ -3,8 +3,20 @@ rose_gold = [246/255,170/255,180/255];
 sea_foam = [0 0.925 0.7];
 burgundy = [0.502,0,0.125];
 gold = [255/255 215/255 0];
+
+color_matrix = [0 0.925 0.7;...
+                        246/255,170/255,180/255;...
+                        255/255 215/255 0;...
+                        0.502,0,0.125;...
+                        190/255,186/255,218/255;...
+                        128/255,177/255,211/255;...
+                        253/255,180/255,98/255;...
+                        179/255,222/255,105/255;...
+                        217/255,217/255,217/255;...
+                        188/255,128/255,189/255]; 
+                        
 make_figs = true;
-save_figs = true;
+save_figs = false;
 formats = {'png','epsc','fig'};
 
 %% messing around with filepaths
@@ -18,11 +30,12 @@ pathname_out = SubfolderMaker(zoom_path,'Figures/');
 pathname_out = PathSlashCorrector(pathname_out);
 
 %% Data sets upon which to run code
-nGroups = [2,5,10];
-nCells = [1000,10000];
-nGenes = [5000, 1000]
-methods = {'_sI','_ALRA','_merged','_sI_thresh','_ALRA_hack'};
+nGroups = 10; %[2,5,10];
+nCells = 10000;% [1000,10000];
+nGenes =  1000;%[5000, 1000]
+methods = {'_sI','_ALRA','_merged','_sI_thresh','_ALRA_hack','_truth','_data'};
 
+method_names = {'softImpute','ALRA','merged','softImpute thresh','ALRA zeros','Ground Truth','Observed'};
 %% Load tSNE and operate on htem
 for i = nGroups
     for j = 1:length(nCells)
@@ -44,24 +57,27 @@ for i = nGroups
             
             filepath_out = SubfolderMaker(pathname_out,dataset_name);
             if make_figs
-                h= figure(1);
+                h= figure;
                 hold on
                 for el = 1:i
+                    color_mixed = color_matrix(el,:);
                     gp_inds = (group_list == el);
-                    plot(tSNE_vals(gp_inds,1),tSNE_vals(gp_inds,2),'o','MarkerSize',10)
+                    plot(tSNE_vals(gp_inds,1),tSNE_vals(gp_inds,2),'.','MarkerSize',12,'Color',color_mixed)
                 end
                 set(gca,'FontName','Helvetica Neue','FontSize',20,'FontWeight','Bold')
                 set(gca,'TickDir','out');
-                title(char(['2-dim t-SNE with true color, method',methods{k}(2:end)]))
+                title(char(['2 dim t-SNE of ',method_names{k},', colored by truth']))
                 
                 % save plots in various figure formats
                 if save_figs
                     for em = 1:length(formats)
-                        save_name = [filepath_out,'tSNE_vis'];
+                        save_name = [filepath_out,'tSNE_vis',methods{k}];
                         saveas(h,save_name,char(formats(em)));
                     end
-                    close all
+                else 
+%                     pause
                 end
+%                 close all
             end
             
         end
